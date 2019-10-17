81359afd6acd3ef47b0218ebc9dacb632630d8e3
Neiso <djulian.ouatla@outlook.com>
2019-10-17 11:13:30 +0200

    fixing atoi and itoa

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: douatla <douatla@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 08:51:51 by douatla           #+#    #+#             */
-/*   Updated: 2019/10/15 18:15:11 by douatla          ###   ########.fr       */
+/*   Updated: 2019/10/10 16:15:09 by douatla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,18 @@ int				ft_atoi(const char *str)
 	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
 				|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
 		i++;
-	if (str[i] == '+' || str[i] == '-')
-	{
-		sign = str[i] == '-' ? 1 : 0;
-		i++;
-	}
+	(str[i] == '-') ? sign++, i++ : nbr;
+	(str[i] == '+') ? i++ : nbr;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		nbr = (nbr * 10) + (str[i] - '0');
 		i++;
-		if (nbr > 2147483648 && sign == 1)
-			return (0);
-		if (nbr > 2147483647 && sign == 0)
+		if (nbr == 2147483648)
+			return (-2147483648);
+		if (nbr > 2147483647 && (sign % 2) == 0)
 			return (-1);
+		else if (nbr > 2147483647)
+			return (0);
 	}
 	return (sign ? (-(nbr)) : (nbr));
 }
